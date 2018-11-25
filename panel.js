b0c7ca1765bf8689796906269497de6c24f41b67
AhmetEsad <38183175+AhmetEsad@users.noreply.github.com>
2018-11-25 19:14:03 +0200

    Create panel.js

---
+++ panel.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron');
+let mainWindow;
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 410, height: 300});
+  mainWindow.loadFile('panel.html');
+  mainWindow.setMenu(null);
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+};
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  };
+});
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  };
+});
